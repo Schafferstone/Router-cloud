7fbd7c5022fea9a194e8c9d221f4706bdc98f4f3
Schafferstone <qianfeng@westke.local>
2014-06-29 17:23:45 +0800

    Initial Commit

---
+++ Router cloud/PSAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  PSAppDelegate.h
+//  Router cloud
+//
+//  Created by Zpz on 14-6-29.
+//  Copyright (c) 2014年 west. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PSAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Router cloud/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Router cloud
+//
+//  Created by Zpz on 14-6-29.
+//  Copyright (c) 2014年 west. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "PSAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([PSAppDelegate class]));
+    }
+}
